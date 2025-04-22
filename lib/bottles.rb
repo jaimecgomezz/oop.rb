4289021823362fd8adb49100cf2d30bf3a64ba5d
Omar Alba <cesarlionelflores@gmail.com>
2025-04-22 16:23:35 -0600

    Methods names modified, first version trying my own logic

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,36 @@
-class Bottles
+class Bottles 
+  def self.song(start,finish)     
+       start.downto(finish) do |verseNumber|
+           puts writeVerse(verseNumber) 
+       end        
+  end
 
-
+  def self.writeVerse(number)
+      case number
+      when 0 
+        "No more bottles of beer on the wall, " +
+        "no more bottles of beer.\n" +
+        "Go to a the store and buy some more, " +
+        "99 bottles of beer on the wall.\n"
+      when 1
+        "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+        "no more bottles of beer on the wall.\n"
+      when 2
+        "2 bottles of beer on the wall, " +
+        "2 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n"
+      else
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number-1} bottles of beer on the wall.\n"
+      end  
+  end
 end
 
-sing = Bottles.new
-puts sing.song
+Bottles.song(99,0)
 
 

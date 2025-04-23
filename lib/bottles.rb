efd5c3538a6c9553bc2c79fed6ad594934b3bfbb
laura <lauvagar41012@gmail.com>
2025-04-22 18:40:10 -0600

    Reestructura método verse usando if/elsif/else para mejorar legibilidad y reducir interpolación

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -3,30 +3,23 @@ class Bottles
     verses(99, 0)
   end
 
-  def verses(starting, ending)
-    starting.downto(ending).map { |n| verse(n) }.join("\n")
+  def verses(initial, final)
+    initial.downto(final).map { |i| verse(i) }.join("\n")
   end
 
-  def verse(n)
-    "#{n == 0 ? 'No more' : n} bottle#{'s' if n != 1}" +
-      ' of beer on the wall, ' +
-      "#{n == 0 ? 'no more' : n} bottle#{'s' if n != 1} of beer.\n" +
-      "#{if n > 0
-           "Take #{n > 1 ? 'one' : 'it'} down and pass it around"
-         else
-           'Go to the store and buy some more'
-         end}, " +
-      "#{if n - 1 < 0
-           99
-         else
-           n - 1 == 0 ? 'no more' : n - 1
-         end} bottle#{'s' if n - 1 != 1}" +
-      " of beer on the wall.\n"
+  def verse(number)
+    if number == 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    elsif number == 1
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif number == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+        "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+    end
   end
 end
-
-
-
-bottles = Bottles.new
-puts "Prueba"
-puts bottles.verses(99, 0)
